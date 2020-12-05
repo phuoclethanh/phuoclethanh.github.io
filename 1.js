9906392d51b67f31876bdc1690c3bfb77a706923
Phuoc <lethanhphuocdktd@gmail.com>
2020-12-05 20:07:31 +0700

    update san pham

--- 1.js
+++ 1.js
@@ -1,6 +1,54 @@
 ﻿
- $(function(){
+//  $(function(){
 
+//  	$('.sanpham .content ul').isotope({
+// 	itemSelector: 'li'
+// 	}); 
+		
+
+//  	$('.sanpham .danhsach nav ul li a').click(function(event) {
+//  		var danhsach = $(this).data('class');
+//  		console.log(danhsach);
+
+//  		if (danhsach == '.all') {
+		
+// 		$('.sanpham .content ul').isotope({filter:'*'});
+// 		}
+// 		else {
+		
+// 			$('.sanpham .content ul').isotope({filter:danhsach});
+// 		}
+
+//  		return false;
+//  	});
+
+//  	$('.gt').click(function(event) {
+//  		$('body, html').animate({scrollTop:$('#khoigt').offset().top},2000);
+//  		return false;
+//  	});
+
+//  	$('.xuong, .kn').click(function(event) {
+//  		$('body, html').animate({scrollTop:$('#khoiedu').offset().top},2000);
+//  		return false;
+//  	});
+//  	$('.sp').click(function(event) {
+//  		$('body, html').animate({scrollTop:$('#khoisp').offset().top},2000);
+//  		return false;
+//  	});
+//  	$('.ttlh').click(function(event) {
+//  		$('body, html').animate({scrollTop:$('#khoittlienhe').offset().top},2000);
+//  		return false;
+//  	});
+ 	
+//  	$('.cm').click(function(event) {
+//  		$('body, html').animate({scrollTop:$('#phanhoi').offset().top},2000);
+//  		return false;
+//  	});
+
+
+// }) 
+
+document.addEventListener("DOMContentLoaded",function(){
  	$('.sanpham .content ul').isotope({
 	itemSelector: 'li'
 	}); 
@@ -23,29 +71,28 @@
  	});
 
  	$('.gt').click(function(event) {
- 		$('body, html').animate({scrollTop:$('#khoigt').offset().top},2000);
+ 		$('body, html').animate({scrollTop:$('#khoigt').offset().top},1000);
  		return false;
  	});
 
  	$('.xuong, .kn').click(function(event) {
- 		$('body, html').animate({scrollTop:$('#khoiedu').offset().top},2000);
+ 		$('body, html').animate({scrollTop:$('#khoiedu').offset().top},1000);
  		return false;
  	});
  	$('.sp').click(function(event) {
- 		$('body, html').animate({scrollTop:$('#khoisp').offset().top},2000);
+ 		$('body, html').animate({scrollTop:$('#khoisp').offset().top},1000);
  		return false;
  	});
  	$('.ttlh').click(function(event) {
- 		$('body, html').animate({scrollTop:$('#khoittlienhe').offset().top},2000);
+ 		$('body, html').animate({scrollTop:$('#khoittlienhe').offset().top},1000);
  		return false;
  	});
  	
  	$('.cm').click(function(event) {
- 		$('body, html').animate({scrollTop:$('#phanhoi').offset().top},2000);
+ 		$('body, html').animate({scrollTop:$('#phanhoi').offset().top},1000);
  		return false;
  	});
 
-
-})  
+ },false)  
 
  
